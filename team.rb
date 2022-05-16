500c47785ae96a35b9612493fab143069b72273d
naoya.matsumoto <naowork12@gmail.com>
2022-05-16 10:38:47 +0000

    m

--- team.rb
+++ team.rb
@@ -9,28 +9,28 @@ class Team
     self.draw = draw
   end
   
-  def calc_win_rate(win, lose)
+  def calc_win_rate
     return self.win.to_f / (self.win + self.lose)
   end
   
-  def show_team_result(name, win, lose, draw)
-    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{calc_win_rate(win, lose)}です。"
+  def show_team_result
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{calc_win_rate}です。"
   end
 end
  
-Team_Giants = Team.new("Giants", 67, 45, 8)
-Team_Tigers = Team.new("Tigers", 60, 53, 7)
-Team_Dragons = Team.new("Dragons", 60, 55, 5)
-Team_BayStars = Team.new("BayStars", 56, 58, 6)
-Team_Carp = Team.new("Carp", 52, 56, 12)
-Team_Swallows = Team.new("Swallows", 41, 69, 10)
+team_giants = Team.new("Giants", 67, 45, 8)
+team_tigers = Team.new("Tigers", 60, 53, 7)
+team_dragons = Team.new("Dragons", 60, 55, 5)
+team_baystars = Team.new("BayStars", 56, 58, 6)
+team_carp = Team.new("Carp", 52, 56, 12)
+team_swallows = Team.new("Swallows", 41, 69, 10)
 
-Team_Giants.show_team_result("Giants", 67, 45, 8)
-Team_Tigers.show_team_result("Tigers", 60, 53, 7)
-Team_Dragons.show_team_result("Dragons", 60, 55, 5)
-Team_BayStars.show_team_result("BayStars", 56, 58, 6)
-Team_Carp.show_team_result("Carp", 52, 56, 12)
-Team_Swallows.show_team_result("Swallows", 41, 69, 10)
+team_giants.show_team_result
+team_tigers.show_team_result
+team_dragons.show_team_result
+team_baystars.show_team_result
+team_carp.show_team_result
+team_swallows.show_team_result
 
 
 
